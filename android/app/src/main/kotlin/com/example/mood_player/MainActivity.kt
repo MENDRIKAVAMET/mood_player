7f484eec7aff77a727cc382a496f0e72ee7dd462
mendrika <mendrikavamet@gmail.com>
2026-08-11 09:00:34 +0300

    Résolution de lecture, next, prev, pause, liste d'attente

--- android/app/src/main/kotlin/com/example/mood_player/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mood_player/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.mood_player
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : AudioServiceActivity()
