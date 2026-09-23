211067718806b439cda2646094e85679ce00d0ad
mendrika <mendrikavamet@gmail.com>
2026-09-24 00:25:10 +0300

    Fix errors

--- lib/widgets/period_mixes_section.dart
+++ lib/widgets/period_mixes_section.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide DayPeriod;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../features/foryou/suggestion_mix_screen.dart';
+import '../models/track.dart';
 import '../providers/mood_suggestions_provider.dart';
 import '../theme/app_theme.dart';
 import 'period_moods_sheet.dart';
